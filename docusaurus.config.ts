4218d22abb09531bd266701408e5a4b5da0f494f
autobore <social@autobore.com>
2025-01-27 00:41:16 -0500

    Customized with Vave Cafe details

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,8 +5,8 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Vave Cafe',
+  tagline: 'AI Automation Agency',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
